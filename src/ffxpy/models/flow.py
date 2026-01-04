e6f62db6b24bfc2a91636b50be73f898d2cd624a
chesham <c.moweb@gmail.com>
2026-01-05 03:47:41 +0800

    feat: allow Flow to propagate settings to jobs

--- src/ffxpy/models/flow.py
+++ src/ffxpy/models/flow.py
@@ -11,4 +11,15 @@ class FlowJob(pydantic.BaseModel):
 
 
 class Flow(pydantic.BaseModel):
+    setting: Setting | None = None
     jobs: list[FlowJob] = pydantic.Field(default_factory=list)
+
+    @pydantic.model_validator(mode='after')
+    def validator(self):
+        if self.setting:
+            parent_setting = self.setting.model_dump(exclude_unset=True)
+            for job in self.jobs:
+                job.setting = Setting.model_validate(
+                    parent_setting | job.setting.model_dump(exclude_unset=True)
+                )
+        return self
